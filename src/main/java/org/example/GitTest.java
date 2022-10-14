9cb8c55933f0631a094fe7e874a84687c9bec232
yusheng <yusheng@126.com>
2022-10-14 15:27:44 +0800

    hot-fix first commit

--- src/main/java/org/example/GitTest.java
+++ src/main/java/org/example/GitTest.java
@@ -7,6 +7,6 @@ public class GitTest {
         System.out.println("Hello world!");
         System.out.println("hello git2");
         System.out.println("hello git3");
-
+        System.out.println("hello git4");
     }
 }
