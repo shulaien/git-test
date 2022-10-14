6ad05927e6b1b44b6ff6ed0b87c231dfff7b02de
yusheng <yusheng@126.com>
2022-10-14 11:26:31 +0800

    first commit

---
+++ src/main/java/org/example/GitTest.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class GitTest {
+    public static void main(String[] args) {
+//        "hello".sout + Enter
+        System.out.println("hello git!");
+        System.out.println("Hello world!");
+    }
+}
