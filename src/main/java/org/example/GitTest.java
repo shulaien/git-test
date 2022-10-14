a1f92611abad11e0cde57996818273b4842d88c6
yusheng <yusheng@126.com>
2022-10-14 15:20:38 +0800

    forth commit

--- src/main/java/org/example/GitTest.java
+++ src/main/java/org/example/GitTest.java
@@ -6,7 +6,7 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("Hello world!");
         System.out.println("hello git2");
-        System.out.println("hello git2");
+        System.out.println("hello git3");
 
     }
 }
